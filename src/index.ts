dbdc8937e12eb29948c83dc95af39891f8fc70c0
Yulia Lepner <ylepner@gmail.com>
2022-02-02 18:41:32 +0100

    feat: add basic layout

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 import './style.scss';
+import { renderTextbook } from './textbook';
+
+function goToTextBook(): void {
+  document.body.innerHTML = '';
+  document.body.appendChild(renderTextbook());
+}
+
+goToTextBook();
--- src/textbook/index.ts
+++ src/textbook/index.ts
@@ -1,2 +1,8 @@
 import html from './index.html';
 import './style.css';
+
+export function renderTextbook(): HTMLDivElement {
+  const template = document.createElement('div');
+  template.innerHTML = html;
+  return template;
+}
