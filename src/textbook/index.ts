1da780bc58c988c66f96863571d766f85d50e9ed
Yulia Lepner <ylepner@gmail.com>
2022-02-02 21:18:38 +0100

    feat: add words rendering

--- src/textbook/index.ts
+++ src/textbook/index.ts
@@ -1,9 +1,19 @@
 import html from './index.html';
 import './style.css';
+import { renderWord } from './word';
 
 export function renderTextbook(): HTMLDivElement {
   const template = document.createElement('div');
   template.innerHTML = html;
-
+  const levelBtn = template.querySelectorAll('.level');
+  const words = template.querySelector('.words') as HTMLElement;
+  levelBtn.forEach((el) => {
+    el.addEventListener('click', () => {
+      words.innerText = '';
+      for (let i = 0; i < 20; i++) {
+        words?.appendChild(renderWord());
+      }
+    });
+  });
   return template;
 }
