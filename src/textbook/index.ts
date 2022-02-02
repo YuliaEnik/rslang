820b0f185a8063bf87d8ec2934c24eb32342a9b3
Yulia Lepner <ylepner@gmail.com>
2022-02-02 22:36:54 +0100

    feat: add render function to word-card component

--- src/textbook/index.ts
+++ src/textbook/index.ts
@@ -1,18 +1,24 @@
 import html from './index.html';
 import './style.css';
 import { renderWord } from './word';
+import { renderWordCard } from './word-card';
 
 export function renderTextbook(): HTMLDivElement {
   const template = document.createElement('div');
   template.innerHTML = html;
   const levelBtn = template.querySelectorAll('.level');
   const words = template.querySelector('.words') as HTMLElement;
+  const wordCard = template.querySelector('.word-card') as HTMLElement;
+  function renderCard() {
+    wordCard.appendChild(renderWordCard());
+    wordCard.classList.add('active');
+  }
   function renderWords() {
     for (let i = 0; i < 20; i++) {
-      words?.appendChild(renderWord({ word: 'flower', onclick: () => { console.log(1111) } }));
+      words?.appendChild(renderWord({ word: 'flower', onclick: () => { renderCard() } }));
     }
   }
-  renderWords();
+  // enderWords();
   levelBtn.forEach((el) => {
     el.addEventListener('click', () => {
       words.innerText = '';
