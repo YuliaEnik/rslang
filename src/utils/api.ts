e22b21580f480139ba664d0f1deeb147f80246ab
Yulia Lepner <ylepner@gmail.com>
2022-02-04 23:11:44 +0100

    feat: change interface naming

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-import { Word } from './types';
+import { IWord } from './types';
 
 const API_ENDPOINT = 'http://localhost:3000';
 
@@ -10,7 +10,7 @@ function buildGetParams(params?: { [key: string]: string | number }) {
   return `?${queryString}`;
 }
 
-export async function getWords(req?: { group: number, page: number }): Promise<Word[]> {
+export async function getWords(req?: { group: number, page: number }): Promise<IWord[]> {
   const result = await fetch(`${API_ENDPOINT}/words/${buildGetParams(req)}`);
   const data = await result.json();
   return data;
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,4 @@
-export interface Word {
+export interface IWord {
   id: string;
   group: number;
   page: number;
