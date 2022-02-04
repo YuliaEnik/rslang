20d244d20e51a42a07e57ea8a9b7c9e1217bb9a5
Yulia Lepner <ylepner@gmail.com>
2022-02-04 22:49:55 +0100

    feat: add types.ts

---
+++ src/utils/types.ts
@@ -0,0 +1,16 @@
+export interface Word {
+  id: string;
+  group: number;
+  page: number;
+  word: string;
+  image: string;
+  audio: string;
+  audioMeaning: string;
+  audioExample: string;
+  textMeaning: string;
+  textExample: string;
+  transcription: string;
+  textExampleTranslate: string;
+  textMeaningTranslate: string;
+  wordTranslate: string;
+}
