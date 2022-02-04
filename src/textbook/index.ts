cdba6e18a60d07c0ae58cf9729d7012026d10a55
Yulia Lepner <ylepner@gmail.com>
2022-02-04 20:00:10 +0100

    feat: add getWords function

--- src/textbook/index.ts
+++ src/textbook/index.ts
@@ -3,7 +3,7 @@ import './style.scss';
 import { renderWord } from './word';
 import { renderWordCard } from './word-card';
 
-export function renderTextbook(): HTMLDivElement {
+export async function renderTextbook(): Promise<HTMLDivElement> {
   const template = document.createElement('div');
   template.innerHTML = html;
   const levelBtn = template.querySelectorAll('.level');
@@ -26,5 +26,41 @@ export function renderTextbook(): HTMLDivElement {
       renderWords();
     });
   });
+  async function getWords(): Promise<void> {
+    const result = await fetch('http://localhost:3000/words/', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await result.json();
+    return data;
+  }
+
+  async function getApiWords() {
+    const wordsData = await getWords();
+    return wordsData;
+  }
+
+  const data = getApiWords();
+  console.log(data);
+
   return template;
 }
+
+// interface Word {
+//   'id': string;
+//   'group': number;
+//   'page': number;
+//   'word': string;
+//   'image': string;
+//   'audio': string;
+//   'audioMeaning': string;
+//   'audioExample': string;
+//   'textMeaning': string;
+//   'textExample': string;
+//   'transcription': string;
+//   'textExampleTranslate': string;
+//   'textMeaningTranslate': string;
+//   'wordTranslate': string;
+// }
