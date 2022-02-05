ec98eb89675d9a362072e1b3250fb7052131f999
Yulia Lepner <ylepner@gmail.com>
2022-02-05 22:46:13 +0100

    feat: delete card-info element

--- src/pages/textbook/word-card/index.ts
+++
@@ -1,7 +0,0 @@
-import html from './index.html';
-
-export function renderWordCard(): HTMLDivElement {
-  const template = document.createElement('div');
-  template.innerHTML = html;
-  return template.children[0] as HTMLDivElement;
-}
