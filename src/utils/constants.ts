678783ee26781c6a72ea5f19c9bdcd5e8ee83112
Marie Serykh <marie.serykh@gmail.com>
2022-02-07 00:51:34 +0100

    feat: #15 add the constant with pages

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,3 +7,36 @@ export const stateTextContentEn: StateTextContentEn = {
 };
 
 export const API_ENDPOINT = 'http://localhost:3000';
+
+export const pages: Page[] = [
+  {
+    title: 'Main',
+    link: '/',
+    type: 'main',
+  },
+  {
+    title: 'Dictionary',
+    link: '/dictionary/1',
+    type: 'dictionary',
+  },
+  {
+    title: 'Sprint',
+    link: '/games',
+    type: 'games',
+  },
+  {
+    title: 'Statistics',
+    link: '/statistics',
+    type: 'statistics',
+  },
+  {
+    title: 'Developers',
+    link: '/developers',
+    type: 'developers',
+  },
+  {
+    title: 'Settings',
+    link: '/settings',
+    type: 'settings',
+  },
+];
