67150c604a2fef296e4c4eb5ee0fdcc7590aee9d
Yulia Lepner <ylepner@gmail.com>
2022-02-12 14:21:03 +0100

    feat: comment out the first version of layout

--- src/pages/textbook/index.ts
+++ src/pages/textbook/index.ts
@@ -1,6 +1,6 @@
 import { getWords } from '../../utils/api';
 import { AppState, UserState } from '../../utils/types';
-import html from './index.html';
+import html from './textbook.html';
 import './style.scss';
 import { renderWord } from './word';
 
@@ -11,71 +11,71 @@ function applyAuthentication(levelButton: HTMLElement, userState: UserState | nu
 }
 
 export function buildTextbook(appState: AppState): HTMLDivElement {
-  let currentPage = 0;
-  let group = 0;
+  // let currentPage = 0;
+  // let group = 0;
   const template = document.createElement('div');
   template.innerHTML = html;
-  const levelButtons = template.querySelectorAll('.level');
-  const words = template.querySelector('.words') as HTMLElement;
-  const wordCard = template.querySelector('.word-card') as HTMLElement;
+  // const levelButtons = template.querySelectorAll('.level');
+  // const words = template.querySelector('.words') as HTMLElement;
+  // const wordCard = template.querySelector('.word-card') as HTMLElement;
 
-  // add options to select
-  const pageSelector = template.querySelector('.page-selector') as HTMLSelectElement;
-  for (let i = 0; i < 30; i++) {
-    const option = document.createElement('option') as HTMLOptionElement;
-    option.value = String(i + 1);
-    option.innerHTML = option.value;
-    pageSelector.appendChild(option);
-  }
+  // // add options to select
+  // const pageSelector = template.querySelector('.select__list') as HTMLInputElement;
+  // for (let i = 0; i < 30; i++) {
+  //   const option = document.createElement('li') as HTMLLIElement;
+  //   option.classList.add('.select__item');
+  //   option.innerText = String(i + 1);
+  //   pageSelector.appendChild(option);
+  // }
 
-  function renderCard() {
-    wordCard.innerHTML = '';
-    wordCard.classList.add('active');
-  }
+  // function renderCard() {
+  //   wordCard.innerHTML = '';
+  //   wordCard.classList.add('active');
+  // }
 
-  function renderWordsList(id: number, page: number) {
-    words.innerHTML = '';
-    getWords({ group: id, page }).then((wordsData) => {
-      // console.log(wordsData);
-      wordsData.forEach((wordEl) => {
-        words?.appendChild(renderWord({ word: wordEl, onclick: renderCard }, appState.user));
-      });
-    });
-  }
-  renderWordsList(group, currentPage);
+  // function renderWordsList(id: number, page: number) {
+  //   words.innerHTML = '';
+  //   getWords({ group: id, page }).then((wordsData) => {
+  //     // console.log(wordsData);
+  //     wordsData.forEach((wordEl) => {
+  //       words?.appendChild(renderWord({ word: wordEl, onclick: renderCard }, appState.user));
+  //     });
+  //   });
+  // }
+  // renderWordsList(group, currentPage);
 
-  levelButtons.forEach((el) => {
-    const levelBtnEl = el as HTMLElement;
-    applyAuthentication(levelBtnEl, appState.user);
-    el.addEventListener('click', () => {
-      words.innerText = '';
-      group = Number(levelBtnEl.dataset.level);
-      renderWordsList(group, currentPage);
-    });
-  });
+  // levelButtons.forEach((el) => {
+  //   const levelBtnEl = el as HTMLElement;
+  //   applyAuthentication(levelBtnEl, appState.user);
+  //   el.addEventListener('click', () => {
+  //     words.innerText = '';
+  //     group = Number(levelBtnEl.dataset.level);
+  //     renderWordsList(group, currentPage);
+  //   });
+  // });
 
-  pageSelector.addEventListener('change', () => {
-    currentPage = Number(pageSelector.value) - 1;
-    renderWordsList(group, currentPage);
-  });
+  // pageSelector.addEventListener('change', () => {
+  //   currentPage = Number(pageSelector.value) - 1;
+  //   renderWordsList(group, currentPage);
+  // });
 
-  const prevBtn = template.querySelector('.previous-btn') as HTMLButtonElement;
-  const nextBtn = template.querySelector('.next-btn') as HTMLButtonElement;
-  prevBtn.addEventListener('click', () => {
-    if (currentPage > 0) {
-      currentPage -= 1;
-      pageSelector.value = String(currentPage + 1);
-      renderWordsList(group, currentPage);
-    }
-  });
+  // const prevBtn = template.querySelector('.btn--prev') as HTMLButtonElement;
+  // const nextBtn = template.querySelector('.btn--next') as HTMLButtonElement;
+  // prevBtn.addEventListener('click', () => {
+  //   if (currentPage > 0) {
+  //     currentPage -= 1;
+  //     pageSelector.value = String(currentPage + 1);
+  //     renderWordsList(group, currentPage);
+  //   }
+  // });
 
-  nextBtn.addEventListener('click', () => {
-    if (currentPage < 29) {
-      currentPage += 1;
-      pageSelector.value = String(currentPage + 1);
-      renderWordsList(group, currentPage);
-    }
-  });
+  // nextBtn.addEventListener('click', () => {
+  //   if (currentPage < 29) {
+  //     currentPage += 1;
+  //     pageSelector.value = String(currentPage + 1);
+  //     renderWordsList(group, currentPage);
+  //   }
+  // });
 
   return template;
 }
