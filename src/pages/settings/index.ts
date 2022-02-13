96c76b89d947895578428a0d504ae8feefb58121
Marie Serykh <marie.serykh@gmail.com>
2022-02-13 22:53:25 +0100

    refactior: #46 delete settings page

--- src/pages/settings/index.ts
+++
@@ -1,7 +0,0 @@
-import { createElement } from '../../utils/utils';
-
-export const buildSettingsPage = (): HTMLElement => {
-  const result = createElement('h1', { class: '' });
-  result.textContent = 'Main';
-  return result;
-};
