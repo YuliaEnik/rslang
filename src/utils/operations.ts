bc8d05ebb1d45641555e7f9442997e8a0af6c98f
Yulia Lepner <ylepner@gmail.com>
2022-02-15 22:20:05 +0100

    feat: add removeWordFromDifficult and convertWordFromAggregated functions

--- src/utils/operations.ts
+++ src/utils/operations.ts
@@ -1,9 +1,35 @@
 import { appState } from '../app';
-import { createUserWord, getWord, updateUserWord } from './api';
-import { UserState, UserWord } from './types';
+import { createUserWord, getAggregatedWords, getWord, getWords, updateUserWord } from './api';
+import { UserState, UserWord, Word, WordFromAggregated } from './types';
+
+function convertWordFromAggregated(wordFromAggregated: WordFromAggregated): Word {
+  return {
+    // eslint-disable-next-line no-underscore-dangle
+    id: wordFromAggregated._id,
+    group: wordFromAggregated.group,
+    page: wordFromAggregated.page,
+    word: wordFromAggregated.word,
+    image: wordFromAggregated.image,
+    audio: wordFromAggregated.audio,
+    audioMeaning: wordFromAggregated.audioMeaning,
+    audioExample: wordFromAggregated.audioExample,
+    textMeaning: wordFromAggregated.textMeaning,
+    textExample: wordFromAggregated.textExample,
+    transcription: wordFromAggregated.transcription,
+    textExampleTranslate: wordFromAggregated.textExampleTranslate,
+    textMeaningTranslate: wordFromAggregated.textMeaningTranslate,
+    wordTranslate: wordFromAggregated.wordTranslate,
+    correctAnswer: undefined,
+    userWord: wordFromAggregated.userWord,
+  };
+}
 
 export function addWordToDifficultList(wordId: string) {
   return createUserWord(appState.user, wordId, { difficulty: 'difficult' });
+}
+
+export function removeWordFromDifficult(wordId: string) {
+  return updateUserWord(appState.user, wordId, { difficulty: 'default' });
 }
 
 export async function addGameResult(userState: UserState | null, wordId: string, body: UserWord) {
@@ -13,4 +39,15 @@ export async function addGameResult(userState: UserState | null, wordId: string,
   } else if (result.ok) {
     updateUserWord(appState.user, wordId, body);
   }
+}
+
+export async function getWordsForRendering(
+  userState: UserState | null, req?: { group: number, page?: number }): Promise<Word[]> {
+  if (!userState?.userId) {
+    const result = await getWords(req);
+    return result;
+  }
+  const result = await getAggregatedWords(userState, req);
+  const convertedWords = result[0].paginatedResults.map((el) => convertWordFromAggregated(el));
+  return convertedWords;
 }
