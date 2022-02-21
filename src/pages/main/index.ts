7b1f509e1424d816a3824de289c90c4e15111b14
Yulia Lepner <ylepner@gmail.com>
2022-02-21 11:10:49 +0100

    feat: delete function to set background

--- src/pages/main/index.ts
+++ src/pages/main/index.ts
@@ -1,27 +1,9 @@
 import './style.scss';
 import html from './index.html';
-import { electron } from 'webpack';
 
 export const buildMainPage = () => {
   const template = document.createElement('div');
   template.innerHTML = html;
-
-  function getRandomColor() {
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    return randomColor;
-  }
-
-  function getRandomPastelColor() {
-    const hue = Math.floor(Math.random() * 360);
-    const randomColor = `hsl(${hue}, 70%, 80%)`;
-    return randomColor;
-  }
-
-  const allSections = template.querySelectorAll('section');
-  allSections.forEach((section: HTMLElement) => {
-    const backgroundColor = getRandomPastelColor();
-    section.style.background = `${backgroundColor}`;
-  });
 
   return template;
 };
